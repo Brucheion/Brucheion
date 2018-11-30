a1f29f1f90d16fc3d33437d69e72bb90bd015c60
ace280 <chitralla@informatik.uni-leipzig.de>
2018-11-30 13:45:43 +0100

    1.1.1.4 -implemented noAuth functionality where user does not need to authenticate with a login provider --implemented noauth flag --adapted template login.html, func main, func loginPost, func InitializeUserDB, func loginGet --added func ValidateNoAuthUser

--- helper.go
+++ helper.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"regexp"
@@ -238,11 +239,6 @@ func InitializeUserDB() error {
 			fmt.Errorf("Failed creating bucket GitLab: %s", err)
 			return err
 		}
-		bucket, err := tx.CreateBucketIfNotExists([]byte("noAuth"))
-		if err != nil {
-			fmt.Errorf("Failed creating bucket noAuth: %s", err)
-			return err
-		}
 
 		_ = bucket //to have done something with the bucket (avoiding 'username declared and not used' error)
 
@@ -279,10 +275,13 @@ func ValidateUserName(username string) *Validation {
 }
 
 func ValidateNoAuthUser(req *http.Request) (*Validation, error) {
-	noAuthUserValidation := &Validation{
-		Message:    "An internal error occured. (This should never happen.)",
-		ErrorCode:  false,
-		BUserInUse: false}
+
+	//prepares the noAuthUserValidation
+	bUserValidation := &Validation{
+		Message:       "An internal error occured. (This should never happen.)",
+		ErrorCode:     false,
+		BUserInUse:    false,
+		BPAssociation: false}
 
 	//get the session to retrieve session/cookie values
 	session, err := GetSession(req)
@@ -293,17 +292,82 @@ func ValidateNoAuthUser(req *http.Request) (*Validation, error) {
 	//get user data from session
 	brucheionUserName, ok := session.Values["BrucheionUserName"].(string)
 	if !ok {
-		fmt.Errorf("Func ValidateUser: Type assertion of brucheionUserName cookie value to string failed or session value could not be retrieved.")
+		fmt.Errorf("Func ValidateNoAuthUser: Type assertion of brucheionUserName cookie value to string failed or session value could not be retrieved.")
 	}
 
+	//open the user database
 	userDB, err := OpenBoltDB(config.UserDB)
 	if err != nil {
 		return nil, err
 	}
 	defer userDB.Close()
+
+	userDB.View(func(tx *bolt.Tx) error {
+
+		//check if Username was used for Providerlogin (if a ProviderUser with that name exists)
+		bucket := tx.Bucket([]byte("GitHub")) //open the GitHub Bucket and test whether there was an entry by this name. There should not be one though. (This is performed to ensure database integrity.)
+		cursor := bucket.Cursor()
+		for userID, PUserName := cursor.First(); userID != nil; userID, PUserName = cursor.Next() { //go through the users bucket and check for BUserName already in use
+			if string(PUserName) == brucheionUserName { //if this Username is associated with an ID in the provider bucket than the database is corrupt in some way..
+				bUserValidation.ErrorCode = false    //Error encountered (PUser in use, but not for this BUser)
+				bUserValidation.BPAssociation = true //brucheionUserName was found in a provider bucket
+			}
+		}
+
+		bucket = tx.Bucket([]byte("GitLab")) //open the GitLab Bucket and test whether there was an entry by this name. There should not be one though. (This is performed to ensure database integrity.)
+		cursor = bucket.Cursor()
+		for userID, PUserName := cursor.First(); userID != nil; userID, PUserName = cursor.Next() { //go through the users bucket and check for BUserName already in use
+			if string(PUserName) == brucheionUserName { //if this Username is associated with an ID in the provider bucket than the database is corrupt in some way..
+				bUserValidation.ErrorCode = false    //Error encountered (PUser in use, but not for this BUser)
+				bUserValidation.BPAssociation = true //brucheionUserName was found in a provider bucket
+			}
+		}
+
+		//check if the username is use already
+		//var brucheionUser BrucheionUser //create a BrucheionUser variable
+		//BUPointer := new(BrucheionUser) //create a pointer that will later be used to check if BrucheionUser is still empty
+		//BUPointer = nil //and make sure that it is empty
+
+		bucket = tx.Bucket([]byte("users"))                                                  //open the user bucket
+		cursor = bucket.Cursor()                                                             //create a cursor object that will be used to iterate over database entries
+		for BUserName, _ := cursor.First(); BUserName != nil; BUserName, _ = cursor.Next() { //go through the users bucket and check for BUserName already in use
+			if string(BUserName) == brucheionUserName { //if this username was found in the users Bucket
+				//buffer := bucket.Get([]byte(brucheionUserName)) //get the brucheionUser as []byte buffer
+				//err := json.Unmarshal(buffer, &brucheionUser)   //unmarshal the buffer and save the brucheionUser in its variable
+				//if err != nil {
+				//	fmt.Println("Func ValidateNoAuthUser: Error unmarshalling brucheionUser: ", err) //this should never happen
+				//}
+				//BUPointer = &brucheionUser //set the pointer to the brucheionuser
+				bUserValidation.BUserInUse = true
+			}
+		}
+
+		if bUserValidation.BUserInUse && bUserValidation.BPAssociation { //if the user was found in the user bucket and a provider bucket (!bUserValidation.ErrorCode) Scenario (1)
+			log.Println("Scenario (1)")
+			bUserValidation.Message = "Username " + brucheionUserName + " is already in use with a provider login. Please choose a different username."
+		} else if bUserValidation.BUserInUse && !bUserValidation.BPAssociation { //if the user was found in the user bucket but not in a provider bucket Scenario (2)
+			log.Println("Scenario (2)")
+			bUserValidation.ErrorCode = true
+			bUserValidation.Message = "NoAuth user " + brucheionUserName + " found. Logged in."
+		} else if !bUserValidation.BUserInUse && !bUserValidation.BPAssociation { //if the user was not found in the user bucket and neither in a provider bucket Scenario (3)
+			log.Println("Scenario (3)")
+			bUserValidation.ErrorCode = true
+			bUserValidation.Message = "New noAuth user " + brucheionUserName + " created. Logged in."
+		}
+
+		return nil //close DB view without an error
+	})
+	return bUserValidation, nil
+
+	/*
+		Check if user was in GH or GL Bucket (not accessable via noAuth ->redirect to login
+		Was in noAuth bucket: redirect
+		Was not in noAuth Bucket: Put user in noAuth Bucket
+	*/
 }
 
 func ValidateUser(req *http.Request) (*Validation, error) {
+
 	bUserValidation := &Validation{
 		Message:      "An internal error occured. (This should never happen.)",
 		ErrorCode:    false,
@@ -335,13 +399,6 @@ func ValidateUser(req *http.Request) (*Validation, error) {
 		fmt.Errorf("Func ValidateUser: Type assertion of ProviderUserID cookie value to string failed or session value could not be retrieved.")
 	}
 
-	/*fmt.Println("Debugging values from session:")
-	fmt.Println("brucheionUserName: " + brucheionUserName)
-	fmt.Println("provider: " + provider)
-	fmt.Println("providerNickName: " + providerNickName)
-	fmt.Println("providerUserID: " + providerUserID)
-
-	fmt.Printf("\nValidateUser: Attempting to open: %s\n", config.UserDB)*/
 	userDB, err := OpenBoltDB(config.UserDB)
 	if err != nil {
 		return nil, err
@@ -351,34 +408,30 @@ func ValidateUser(req *http.Request) (*Validation, error) {
 	userDB.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte("users")) //try to open the user bucket
 
-		var brucheionUser BrucheionUser //create a pointer to a BrucheionUser variable
-		pointer := new(BrucheionUser)
-		cursor := bucket.Cursor()
-		pointer = nil
+		var brucheionUser BrucheionUser //create a BrucheionUser variable
+		BUPointer := new(BrucheionUser) //create a pointer that will later be used to check if BrucheionUser is still empty
+		BUPointer = nil                 //and make sure that it is empty
+		cursor := bucket.Cursor()       //create a cursor object that will be used to iterate over database entries
+
 		for BUserName, _ := cursor.First(); BUserName != nil; BUserName, _ = cursor.Next() { //go through the users bucket and check for BUserName already in use
 			//Login scenario (4)
-			if string(BUserName) == brucheionUserName { //if this userID was in the Bucket
+			if string(BUserName) == brucheionUserName { //if this username was found in the users Bucket
 				buffer := bucket.Get([]byte(brucheionUserName)) //get the brucheionUser as []byte buffer
-				err := json.Unmarshal(buffer, &brucheionUser)   //unmarshal the buffer
+				err := json.Unmarshal(buffer, &brucheionUser)   //unmarshal the buffer and save the brucheionUser in its variable
 				if err != nil {
 					fmt.Println("Error unmarshalling brucheionUser: ", err)
 				}
-				pointer = &brucheionUser //set the pointer to the brucheionuser
-				/*fmt.Println("&brucheionUser")
-				fmt.Println(&brucheionUser)
-				/*fmt.Println("*brucheionUser")
-				fmt.Println(*brucheionUser)*/
+				BUPointer = &brucheionUser //set the pointer to the brucheionuser
 			}
 		}
 
 		//check if ProviderUser was used for other BUser
-		if pointer == nil { //Login scenarios (4), (5)
+		if BUPointer == nil { //Login scenarios (4), (5)
 			bUserValidation.BUserInUse = false   //This BUsername was not in use yet
 			bUserValidation.SameProvider = false //No BUser -> No provider chosen
 			bucket = tx.Bucket([]byte(provider)) //open the provider Bucket
 			cursor := bucket.Cursor()
-			//for userID, nickName := cursor.First(); userID != nil; userID, nickName = cursor.Next() { //go through the users bucket and check for BUserName already in use
-			for userID, _ := cursor.First(); userID != nil; userID, _ = cursor.Next() { //go through the users bucket and check for BUserName already in use
+			for userID, _ := cursor.First(); userID != nil; userID, _ = cursor.Next() { //go through the provider bucket and check for BUserName already in use
 
 				//Login scenario (4)
 				if string(userID) == providerUserID { //if this userID was in the Bucket
@@ -389,10 +442,13 @@ func ValidateUser(req *http.Request) (*Validation, error) {
 			}
 			cursor = nil
 			//Login scenario (5)
+			//TO BE TESTED: alternative if statement: if bUserValidation.BUserInUse == false
+			//&& bUserValidation.SameProvider == false
+			//&& bUserValidation.PUserInUse == false{
 			if bUserValidation.Message == "An internal error occured. (This should never happen.)" { //If userID was not found in DB, message will be unaltered.
 				bUserValidation.Message = "User " + brucheionUserName + " created for login with provider " + provider + ". Login successfull."
 				bUserValidation.ErrorCode = true   //New BUser and new PUser -> Creating a new user is not an error
-				bUserValidation.PUserInUse = false //ProviderUser not in use yet
+				bUserValidation.PUserInUse = false //ProviderUser not in use yet (could be omitted)
 			}
 		} else { //Login Scenarios (1), (2), (3)
 			bUserValidation.BUserInUse = true       //The BrucheionUser has a representation in users.DB
