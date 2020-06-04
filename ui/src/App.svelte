b055b532b1398ca09526cafd0b1e3bef26b4d031
Jan Kaßel <jan@kassel.works>
2020-06-04 16:25:59 +0200

    feat: add svelte routing structure

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,28 +1,12 @@
 <script>
+  import { Router, Link, Route } from 'svelte-routing'
+  import Ingest from './routes/Ingest.svelte'
+  import NotFound from './routes/NotFound.svelte'
+
+  export let url = location.pathname
 </script>
 
-<main>
-  <h1>Hi there!</h1>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<Router url="{url}">
+  <Route path="ingest" component={Ingest}/>
+  <Route path="*" component={NotFound}/>
+</Router>
---
+++ ui/src/routes/Ingest.svelte
@@ -0,0 +1,22 @@
+<script></script>
+
+<div class="container is-fluid">
+  <section>
+    <p>Folio Ingestion</p>
+
+    <form>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label" for="name">Folio Name</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control">
+              <input id="name" class="input" type="text" placeholder="CTS URN" />
+            </p>
+          </div>
+        </div>
+      </div>
+    </form>
+  </section>
+</div>
