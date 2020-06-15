f0407653fe52756ab77dba0b0cdb44a842a64d34
Jan Kaßel <jan@kassel.works>
2020-06-15 14:00:00 +0200

    feat: add ingestion ui logic

---
+++ ui/src/components/Message.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { fade } from 'svelte/transition'
+
+  export let text = ''
+  export let error = false
+</script>
+
+<em class:error transition:fade={{duration: 125}}>{text}</em>
+
+<style>
+  em {
+    display: inline-block;
+    padding: 5px;
+
+    font-style: normal;
+  }
+
+  .error {
+    color: rgb(200, 10, 10);
+  }
+</style>
