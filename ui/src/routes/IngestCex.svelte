b8cd10873da7e605a11b8d38ab7ac7218beb8e56
Jan Kaßel <jan@kassel.works>
2021-01-26 12:23:23 +0100

    feat(ingest/cex): check file existence

--- ui/src/routes/IngestCex.svelte
+++ ui/src/routes/IngestCex.svelte
@@ -1,25 +1,54 @@
 <script>
   import FormLine from '../components/FormLine.svelte'
   import TextInput from '../components/TextInput.svelte'
+  import Message from '../components/Message.svelte'
+  import debounce from '../lib/debounce'
+  import { purename } from '../lib/fs'
 
   let inputRef = undefined
   let cexFile = ''
   let fileName = ''
-  let uploadFile = true
   let complete = false
+  let validName = true
 
   function handleSubmit(event) {
     event.preventDefault()
   }
+
+  async function handleFileName(fileName) {
+    let res
+    try {
+      res = await fetch(`/api/v1/cex/exists?name=${fileName}`)
+    } catch (err) {
+      validName = true
+      return
+    }
+
+    if (res.status !== 200) {
+      validName = false
+      return
+    }
+    const d = await res.json()
+    if (d.status !== 'success' || d.data.exists) {
+      validName = false
+      return
+    }
+    validName = true
+  }
+  const debouncedHandleFileName = debounce(handleFileName, 500)
 
   $: if (cexFile && inputRef) {
     const [file] = inputRef.files
 
     if (file.name.match(/\.cex$/)) {
       if (!fileName) {
-        fileName = file.name
+        fileName = purename(file.name)
       }
     }
+  }
+
+  $: if (!!fileName) {
+    debouncedHandleFileName(fileName)
   }
 </script>
 
@@ -32,6 +61,10 @@
     box-sizing: border-box;
     max-width: 700px;
     padding: 25px;
+  }
+
+  .input-file {
+    padding: 4px 0;
   }
 </style>
 
@@ -52,6 +85,7 @@
               id="cex-file"
               type="file"
               accept=".cex"
+              class="input-file"
               bind:value={cexFile}
               bind:this={inputRef} />
           </FormLine>
@@ -60,11 +94,14 @@
             <TextInput
               id="file-name"
               placeholder="References file name"
-              bind:value={fileName} />
-          </FormLine>
+              bind:value={fileName}
+              invalid={!validName} />
 
-          <FormLine id="upload-file" label="Upload file?">
-            <input type="checkbox" id="upload-file" bind:checked={uploadFile} />
+            {#if !validName}
+              <Message
+                error={true}
+                text="This name is not valid. Please choose a different one." />
+            {/if}
           </FormLine>
 
           <FormLine offset>
