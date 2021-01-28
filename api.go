7405cc16ddde1cc0be209be286c6a012b7801e0a
Jan Kaßel <jan@kassel.works>
2021-01-28 15:25:02 +0100

    feat(api/cex): upload & ingestion handler

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -34,6 +35,60 @@ func handleCEXExists(w http.ResponseWriter, r *http.Request) {
 	respondWithData(w, JSONExistsResponse{Exists: exists}, 200)
 }
 
+// handleCEXUpload reads a CEX file transferred in a POST request into memory
+// and attempts to load the contained CEX data into the user database.
+// adapted example from <https://tutorialedge.net/golang/go-file-upload-tutorial/>
+func handleCEXUpload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.NotFound(w, r)
+		return
+	}
+
+	user, err := getSessionUser(r)
+	if err != nil {
+		http.Error(w, "Unauthorized", 401)
+		return
+	}
+
+	// max. 20mb in size
+	r.ParseMultipartForm(20 << 20)
+	file, handler, err := r.FormFile("cex-file")
+	if err != nil {
+		respondWithError(w, "file_not_found", 400)
+		return
+	} else if filepath.Ext(handler.Filename) != ".cex" {
+		respondWithError(w, "bad_file_ext", 400)
+		return
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Error reading file:\n%s\n", err.Error())
+		respondWithError(w, "bad_file_body", 500)
+		return
+	}
+
+	// loadCEX currently does not particular error cases and thus might panic
+	// on malformed file input. We'll handle any panics here in order to
+	// provide proper responses.
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+			log.Printf("Error loading file:\n%s\n", err.Error())
+			respondWithError(w, "bad_cex_data", 500)
+		}
+	}()
+	err = loadCEX(string(data), user)
+	if err != nil {
+		log.Printf("Error loading file:\n%s\n", err.Error())
+		respondWithError(w, "bad_cex_data", 500)
+		return
+	}
+
+	respondWithSuccess(w)
+}
+
 type JSONResponse struct {
 	Status  string      `json:"status"`
 	Message string      `json:"message"`
@@ -52,6 +107,7 @@ func respondWithData(w http.ResponseWriter, data interface{}, code int) {
 	respondWithJSON(w, "success", "", data, code)
 }
 
+// respondWithJSON responds to a request with a JSON-encoded response of JSONResponse.
 // adapted from <https://stackoverflow.com/a/59764037>
 func respondWithJSON(w http.ResponseWriter, status string, message string, data interface{}, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
@@ -66,8 +122,8 @@ func respondWithJSON(w http.ResponseWriter, status string, message string, data 
 }
 
 // requireSession is a middleware for wrapping an http.HandlerFunc
-//   and checking for a valid user session. The username is then
-//   stored in the request context.
+// and checking for a valid user session. The username is then
+// stored in the request context.
 func requireSession(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		session, err := getSession(r)
@@ -76,16 +132,14 @@ func requireSession(h http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		user, message, loggedIn := testLoginStatus("", session)
+		_, _, loggedIn := testLoginStatus("", session)
 		if !loggedIn {
-			log.Println(message)
 			Logout(w, r)
 			return
 		}
 
 		c := context.WithValue(r.Context(), "session", session)
-		d := context.WithValue(c, "user", user)
-		r = r.WithContext(d)
+		r = r.WithContext(c)
 		h(w, r)
 	}
 }
