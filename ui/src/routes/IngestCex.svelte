c2c0f61202578b18766c72321f0773cce0dd2042
Jan Kaßel <jan@kassel.works>
2021-02-02 12:17:51 +0100

    feat(ingest/cex): error messages and dialogue

--- ui/src/routes/IngestCex.svelte
+++ ui/src/routes/IngestCex.svelte
@@ -1,9 +1,32 @@
 <script>
   import FormLine from '../components/FormLine.svelte'
+  import Message from '../components/Message.svelte'
 
-  let inputRef
+  let inputRef, formRef
   let cexFile = ''
   let complete = false
+  let loading = false
+  let modalVisible = false
+  let errorMessage = null
+
+  const errorMessages = {
+    bad_file_ext:
+      'The submitted file did not have the corresponding .cex file extension.',
+    bad_file_body: 'The submitted file could not be read.',
+    file_not_found: 'The submitted file could not be read.',
+    bad_cex_data:
+      'The CEX data contained erroneous data and could not be processed.',
+    unknown:
+      'An unknown error occurred. This is not necessarily related to the uploaded CEX data',
+  }
+
+  function showModal() {
+    modalVisible = true
+  }
+  function hideModal() {
+    modalVisible = false
+    errorMessage = null
+  }
 
   async function handleSubmit(event) {
     event.preventDefault()
@@ -15,17 +38,29 @@
     const formData = new FormData()
     formData.append('file', file)
 
+    loading = true
     const res = await fetch('/api/v1/cex/upload', {
       method: 'POST',
       body: formData,
     })
+    loading = false
 
-    const data = await res.json()
     if (res.status !== 200) {
-      console.error('HALP', data)
+      try {
+        const data = await res.json()
+        if (typeof errorMessages[data.message] !== 'undefined') {
+          errorMessage = errorMessages[data.message]
+        } else {
+          errorMessage = errorMessages.unknown
+        }
+      } catch (err) {
+        errorMessage = errorMessages.unknown
+      }
     } else {
-      console.log('cool :)', data)
+      formRef.reset()
     }
+
+    showModal()
   }
 
   $: complete = !!cexFile
@@ -47,18 +82,17 @@
   }
 </style>
 
-<div class="container is-fluid">
+<div class="container">
   <section>
-    <div class="columns is-desktop">
-
+    <div class="columns">
       <div class="column form-column">
         <p>
-          Ingest previously created references to set up project on Brucheion.
-          Please select the respective CEX file below.
+          Ingest references created previously to set up a project on Brucheion.
+          Please select the respective CEX file below and submit the file to
+          start the ingestion process.
         </p>
 
-        <form class="form" on:submit={handleSubmit}>
-          <h4 class="title is-4">Media Data</h4>
+        <form class="form" bind:this={formRef} on:submit={handleSubmit}>
           <FormLine id="cex-file" label="CEX File">
             <input
               id="cex-file"
@@ -70,14 +104,47 @@
           </FormLine>
 
           <FormLine offset>
+            <Message
+              text="Processing CEX data may take up to several minutes. Please
+              don't refresh the page while ingestion is in progress." />
+          </FormLine>
+          <FormLine>
             <button
               class="button is-success"
-              disabled={!complete}
+              type="submit"
+              class:is-loading={loading}
+              disabled={!complete || loading}
               on:click={handleSubmit}>
-              Upload CEX File
+              Ingest
             </button>
           </FormLine>
         </form>
+
+        <div class="modal" class:is-active={modalVisible}>
+          <div class="modal-background" on:click={hideModal} />
+          <div class="modal-card">
+            <header class="modal-card-head">
+              <p class="modal-card-title">
+                {#if errorMessage !== null}Error{:else}Ingestion Successful{/if}
+              </p>
+              <button class="delete" aria-label="close" on:click={hideModal} />
+            </header>
+            <section class="modal-card-body">
+              {#if errorMessage !== null}
+                <p>{errorMessage}</p>
+              {:else}
+                <p>The CEX data has been ingested successfully.</p>
+              {/if}
+            </section>
+            <footer class="modal-card-foot">
+              <button class="button" on:click={hideModal}>Dismiss</button>
+            </footer>
+            <button
+              class="modal-close is-large"
+              aria-label="close"
+              on:click={hideModal} />
+          </div>
+        </div>
       </div>
     </div>
   </section>
