3bf062003c3eeaddee9189c918e30511f415b35e
Jan Kaßel <jan@kassel.works>
2021-05-03 12:44:30 +0200

    feat: polish transcription desk

--- ui/src/components/PassageDesk.svelte
+++ ui/src/components/PassageDesk.svelte
@@ -6,9 +6,6 @@
 
   export let passage
 
-  // TODO debug
-  $: console.log('passage', passage)
-
   let previewViewer = undefined,
     viewerOpts = undefined,
     previewVisible = false,
@@ -16,6 +13,9 @@
     selectedImageRef = undefined,
     didMount = false,
     selectedCatalogUrn = passage.catalog.urn
+
+  // FIXME: this is a pretty naive attempt to catch the passage ID
+  $: passageId = passage.id.split(':').pop()
 
   function createViewer(opts) {
     const { tileSources, ...otherOpts } = opts
@@ -34,116 +34,228 @@
     previewViewer.open(tileSources)
   }
 
-  // this should update the folio viewer a) once after mounting and b) when `passage` changes due to reactivity.
-  $: if (didMount) {
-    updateViewer(passage.imageRefs)
-  }
-
   function updateViewer(refs) {
     if (previewViewer) {
       previewViewer.destroy()
     }
 
     if (Array.isArray(refs) && refs.length > 0) {
-      viewerOpts = getInternalOpts('preview', refs[0])
-      selectedImageRef = refs[0]
+      if (!selectedImageRef || !refs.includes(selectedImageRef)) {
+        selectedImageRef = refs[0]
+      }
+      viewerOpts = getInternalOpts('preview', selectedImageRef)
 
       createViewer(viewerOpts)
     }
   }
 
-  function handleWitnessSelection(e) {
-    console.log('witness selected', e.target.value)
-
-    // FIXME: this is a pretty naive attempt to catch the passage ID
-    const p = passage.id.split(':').pop()
-    console.log('passage ID', passage.id, selectedCatalogUrn, p)
-
-    navigate(`/view/${selectedCatalogUrn}${p}`)
+  function handleWitnessSelection() {
+    navigate(`/view/${selectedCatalogUrn}${passageId}`)
   }
 
+  /* this should update the folio viewer a) once after mounting and b) when `passage` changes due to reactivity.
+   * this is just a lazy trick to trigger the viewer update in coordination with svelte's reactivity */
+  $: if (didMount) {
+    updateViewer(passage.imageRefs)
+  }
   onMount(() => {
     didMount = true
   })
 </script>
 
 <style>
+  :global(:root) {
+    --toolbar-bg-color: rgb(240, 240, 240);
+    --toolbar-text-color: rgb(50, 50, 50);
+    --toolbar-border-color: rgb(200, 200, 200);
+  }
+
   .toolbar {
     display: flex;
     flex-direction: row;
+    align-items: center;
+
+    height: 40px;
+    margin: 0;
+    border-top: 1px solid var(--toolbar-border-color);
+    border-bottom: 1px solid var(--toolbar-border-color);
+    padding: 0;
+
+    list-style: none;
+    background: var(--toolbar-bg-color, white);
   }
 
-  .witnesses {
+  .toolbar,
+  .toolbar select {
+    font: 400 14px/100% 'Inter', sans-serif;
+    color: var(--toolbar-text-color, black);
+  }
+
+  .toolbar.stacked-below {
+    border-top: 0;
+  }
+
+  .toolbar li {
+    flex-shrink: 0;
+    margin: 0;
+    padding: 2px 8px;
+  }
+
+  /* join left */
+  .toolbar li.jl {
+    margin-left: 2px;
+  }
+  /* space left */
+  .toolbar li.sl {
+    margin-left: 8px;
+  }
+  /* border left */
+  .toolbar li.bl {
+    border-left: 1px solid var(--toolbar-border-color);
+  }
+
+  .toolbar li:last-child {
+    border-right: 0;
+  }
+
+  .toolbar li label {
+    margin: 0;
+    padding: 0;
+
+    font-weight: 600;
+    font-size: inherit;
+  }
+
+  .toolbar li code {
+    font: 14px/100% 'IBM Plex Mono', monospace;
+    color: inherit;
   }
 
   .desk {
     display: flex;
     flex-direction: column;
+    flex-grow: 1;
+
     width: 100%;
     height: 100%;
+    min-height: 600px;
+  }
+
+  .pane {
+    flex-basis: 50%;
+    flex-shrink: 0;
+    flex-grow: 1;
+  }
+
+  .pane.static {
+    flex-grow: 0;
+  }
+
+  .pane.grow {
+    display: flex;
+    flex-direction: column;
   }
 
   .preview {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 0;
+    flex-shrink: 0;
+
     box-sizing: border-box;
     width: 100%;
-    height: 601px;
-    border: 2px solid rgba(230, 230, 230);
-    border-radius: 4px;
+    height: 450px;
     padding: 4px;
-
     background: rgba(246, 245, 245);
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.15);
+  }
+
+  :global(.openseadragon-container) {
+    flex-grow: 1;
+  }
+
+  .transcription {
+    box-sizing: border-box;
+    padding: 16px;
+    overflow-y: scroll;
   }
 </style>
 
-<div>
-  <nav role="navigation" class="toolbar">
-    <Link to={`/view/${passage.previousPassage}`}>Previous Passage</Link>
-    <Link to={`/view/${passage.nextPassage}`}>Next Passage</Link>
+<nav role="navigation">
+  <ul class="toolbar">
+    <li>
+      <label>Passage</label>
+    </li>
 
-    <form class="witnesses">
-      <div class="control">
-        <div class="select">
-          <select
-            bind:value={selectedCatalogUrn}
-            on:change={handleWitnessSelection}>
-            {#each passage.textRefs as ref}
-              <option value={ref}>{ref}</option>
-            {/each}
-          </select>
-        </div>
-      </div>
-    </form>
-  </nav>
-
-  <div class="desk">
-    <section>
-      <div>
-        <h3>Folio selection</h3>
-        <div class="select">
-          <select bind:value={selectedImageRef}>
-            {#if !!passage.imageRefs}
-              {#each passage.imageRefs as ref}
-                <option value={ref}>{ref}</option>
-              {/each}
-            {:else}
-              <option disabled>No image references</option>
-            {/if}
-          </select>
-        </div>
-      </div>
-      <div id="preview" class="preview" />
-    </section>
-
-    <section>
-      <div class="tile is-ancestor" id="work-row-1">
-        <p>
-          {#each passage.transcriptionLines as line}
-            {line}
-            <br />
+    <li class="sl">
+      <div class="select">
+        <select
+          bind:value={selectedCatalogUrn}
+          on:change={handleWitnessSelection}>
+          {#each passage.textRefs as ref}
+            <option value={ref}>{ref}</option>
           {/each}
-        </p>
+        </select>
       </div>
-    </section>
-  </div>
+    </li>
+    <li class="jl">
+      <code>{passageId}</code>
+    </li>
+
+    <li class="sl">
+      <Link to={`/view/${passage.previousPassage}`}>← Previous Passage</Link>
+    </li>
+    <li class="bl">
+      <Link to={`/view/${passage.nextPassage}`}>Next Passage →</Link>
+    </li>
+
+    <li class="sl">
+      <a href="#">Metadata</a>
+    </li>
+  </ul>
+</nav>
+
+<div class="desk">
+  <section class="pane grow static">
+    <ul class="toolbar stacked-below">
+      <li>
+        <label>Folio</label>
+      </li>
+      <li>
+        <form on:submit|preventDefault={() => console.log('help?')}>
+          <div class="select">
+            <select bind:value={selectedImageRef}>
+              {#if !!passage.imageRefs}
+                {#each passage.imageRefs as ref}
+                  <option value={ref}>{ref}</option>
+                {/each}
+              {:else}
+                <option disabled>No image references</option>
+              {/if}
+            </select>
+          </div>
+        </form>
+      </li>
+
+      <li class="sl">
+        <Link to={`/edit2/${passage.id}`}>Edit References</Link>
+      </li>
+    </ul>
+    <div id="preview" class="preview" />
+  </section>
+
+  <section class="pane">
+    <ul class="toolbar">
+      <li>
+        <label>Transcription</label>
+      </li>
+    </ul>
+    <div class="transcription">
+      <p>
+        {#each passage.transcriptionLines as line}
+          {line}
+          <br />
+        {/each}
+      </p>
+    </div>
+  </section>
 </div>
